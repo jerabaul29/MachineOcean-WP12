a9eaea321b6b582f27f4d0bec85eef43dc582a15
JRT <jean.rblt@gmail.com>
2020-07-10 14:42:16 +0200

    Add several requirements, check for basemap existence and version

--- motools/__init__.py
+++ motools/__init__.py
@@ -26,3 +26,16 @@ logger = logging.getLogger(__name__)
 # Make sure the interpreter is in UTC in all the following
 os.environ["TZ"] = "UTC"
 time.tzset()
+
+# Check that basemap is present and in high enough version.
+# Doing it here rather than in requirements, as basemap is not
+# on Pypi any longer; see: https://matplotlib.org/basemap/users/download.html
+try:
+    basemap_version = None
+    basemap_version_needed = "1.2.1"
+    import mpl_toolkits.basemap as bm
+    basemap_version = bm.__version__
+    assert(basemap_version >= basemap_version_needed)
+except:
+    raise ImportError("Need basemap in a recent version: at least v {}, present is {}; this is not on Pypi any longer. Try with conda or ```sudo apt-get install -y python-mpltoolkits.basemap``` to install.".format(basemap_version_needed, basemap_version))
+
