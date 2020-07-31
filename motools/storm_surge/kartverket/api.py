7200fcc7b0541f92ea67b6c813142189b180f9a4
JRT <jean.rblt@gmail.com>
2020-07-31 16:17:04 +0200

    working

--- motools/storm_surge/kartverket/api.py
+++ motools/storm_surge/kartverket/api.py
@@ -17,7 +17,9 @@ def perform_request(request):
 
 class KartverketAPI(object):
     def __init__(self):
-        pass
+        self.stations_IDs = None
+        self.stations_info_dict = None
+        # TODO: read from conf fill value
 
     def get_stations_info(self):
         request = "http://api.sehavniva.no/tideapi.php?tide_request=stationlist&type=perm"
@@ -31,5 +33,29 @@ class KartverketAPI(object):
             dict_tag = crrt_tag.attrs
             dict_all_stations_info[dict_tag["code"]] = dict_tag
 
+        # TODO: add Time interval of available water level data from this station
+        # TODO: add Year of first and last statistics for station
+
+        self.stations_IDs = list(dict_all_stations_info.keys())
+        self.stations_info_dict = dict_all_stations_info
+
         return(dict_all_stations_info)
+
+    def get_one_station_over_time_extent(self):
+        # TODO: station ID, time start, time stop, frequency
+        # TODO: check that frequency is lower or equal to the available frequency
+        # TODO: check that the time interval is compatible with time extent
+        pass
+
+    def get_all_stations_over_time_extent(self):
+        # TODO: choose sampling frequency, start, end time
+        # TODO: loop through all stations
+        # TODO: check that sampling frequency is ok
+        # TODO: check that time of availability is ok
+        # TODO: populate the data
+
+        pass
+
+# TODO: use a request timer to not overload the server; have it as a helper? / for the whole class
+# TODO: check the water level change, and similar corrections
 
